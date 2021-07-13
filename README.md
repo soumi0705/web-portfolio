24d45ff065d62cf519c7e0e324fc9592ae137da2
soumi0705 <somida00@gmail.com>
2021-07-14 02:46:19 +0530

    Created Web-Portfolio

---
+++ README.md
@@ -0,0 +1,6 @@
+## __My Portfolio__
+
+
+
+
+
---
+++ src/resumeData.js
@@ -0,0 +1,112 @@
+let resumeData = {
+    "imagebaseurl":"",
+    "name": "Soumitro Datta",
+    "role": "Software/ Web-Developer",
+    "linkedinId":" https://www.linkedin.com/in/soumitro-datta-b89919183/",
+    "skypeid": "live:somida00",
+    "roleDescription": " I have a strong grasp of HTML/CSS and Python. Solidity Development and Blockchain DApps have piqued my interest, and I'm presently studying about them.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/soumitro-datta-b89919183/",
+          "className":"fa fa-linkedin linkedIn"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/soumi0705",
+          "className":"fa fa-github github"
+        },
+        {
+          "name":"skype",
+          "url":"https://www.instagram.com/soumi0705/",
+          "className":"fa fa-instagram instagram"
+        }
+      ],
+    "aboutme":"I am currently a final year student at Vellore Institute of Technology, Chennai and pursuing my B.Tech from here. I am a self taught Web Developer, currently diving deeper into Blockchain Devlopment with Solidity.",
+    "address":"Haryana, India",
+    "website":"",
+    "education":[
+      {
+        "UniversityName":"Vellore Institure of Technology, Chennai",
+        "specialization":"B-Tech Computer Science and Engineering",
+        "MonthOfPassing":"Present",
+        "YearOfPassing":"",
+        "Achievements":"Current CGPA: 8.31"
+      },
+      {
+        "UniversityName":"The Lawrence School, Sanawar",
+        "specialization":"PCME with C++",
+        "MonthOfPassing":"March",
+        "YearOfPassing":"2018",
+        "Achievements":"Percentage : 94.2% "
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Ericsson",
+        "specialization":"Summer Internship",
+        "MonthOfLeaving":"May - Jun",
+        "YearOfLeaving":"2019",
+        "Achievements":""
+      },
+    //   {
+    //     "CompanyName":"Some Company",
+    //     "specialization":"Some specialization",
+    //     "MonthOfLeaving":"Jan",
+    //     "YearOfLeaving":"2018",
+    //     "Achievements":"Some Achievements"
+    //   }
+    ],
+    "skillsDescription":"These are some of my top skills",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+	  {
+        "skillname":"Javascript"
+      },
+      {
+        "skillname":"Python"
+      },
+      {
+        "skillname":"Adobe Photoshop"
+      },
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"./images/portfolio/girl.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"./images/portfolio/coffee.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"./images/portfolio/judah.jpg"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"./images/portfolio/origami.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
