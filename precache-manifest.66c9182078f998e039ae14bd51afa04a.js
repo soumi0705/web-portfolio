2eb6b69fb34bbfb178bf300223c5e7f2803b7847
soumi0705 <somida00@gmail.com>
2021-07-14 02:48:08 +0530

    Updates

---
+++ precache-manifest.66c9182078f998e039ae14bd51afa04a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d2fff68408087abe59f6",
+    "url": "/web-portfolio/static/css/main.d8da440e.chunk.css"
+  },
+  {
+    "revision": "d2fff68408087abe59f6",
+    "url": "/web-portfolio/static/js/main.d2fff684.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/web-portfolio/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "48028e5f4d79797ae41f",
+    "url": "/web-portfolio/static/js/runtime~main.48028e5f.js"
+  },
+  {
+    "revision": "f525cee99f62f563f905c88a5856664f",
+    "url": "/web-portfolio/index.html"
+  }
+];
